76dc6d1257c09db4a2a8f1f867b8e2715614836f
Kshitij Gupta <kshitij.gupta2301@gmail.com>
2024-02-28 06:20:57 +0530

    Created header component for transformation pages

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -8,11 +8,11 @@ const Layout = ({children}: { children: React.ReactNode}) => {
       <Sidebar />
       <MobileNav />
       
-        <div className='root-container'>
-            <div className='wrapper'>
-                {children}
-            </div>
-        </div>
+      <div className='root-container'>
+          <div className='wrapper'>
+              {children}
+          </div>
+      </div>
     </main>
   )
 }
